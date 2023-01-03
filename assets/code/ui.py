6fd991af4c93e74d151ec15d820fdb58b8c6a298
JL Houss <67522466+jl-houss@users.noreply.github.com>
2023-01-03 21:05:42 +0100

    style: add colors file

---
+++ assets/code/ui.py
@@ -0,0 +1,14 @@
+class Colors:
+    Mandarin = "EF8354"
+    Sepia = "794412"
+    Liver = "644F4F"
+    
+    White = "FFFFFF"
+    Silver = "BFC0C0"
+    Coral = "4F5D75"
+    Cadet = "2D3142"
+
+    Success     = "#198754"
+    Warning     = "#EED202"
+    Danger      = "#FF002D"
+    
