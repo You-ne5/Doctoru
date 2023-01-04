4b66f9c37ddb3bd6bbe492dc51a52605c419c13a
You-ne5 <erenyounes2006@gmail.com>
2023-01-04 14:28:24 +0100

    style: add login page gui

--- assets/code/ui.py
+++ assets/code/ui.py
@@ -1,3 +1,5 @@
+
+
 class Colors:
     Mandarin = "#EF8354"
     Sepia = "#794412"
@@ -11,4 +13,17 @@ class Colors:
     Success     = "#198754"
     Warning     = "#EED202"
     Danger      = "#FF002D"
+
+def clear(frame):
+    for widget in frame.winfo_children():
+        widget.destroy()
+
+
+def center(w, h, frame):
+    ws = frame.winfo_screenwidth()
+    hs = frame.winfo_screenheight()
+    x = (ws / 2) - (w / 2)
+    y = (hs / 2) - (h / 2)
+    frame.geometry("%dx%d+%d+%d" % (w, h, x, y))
+    clear(frame)
     
--- main.py
+++ main.py
@@ -3,17 +3,19 @@ from os import environ as env
 
 from customtkinter import CTk
 
-from assets.code.ui import Colors
+from assets.code.ui import Colors, center
 from src.auth.login import LoginPage
 
 
 class App(CTk):
     def __init__(self) -> None:
-        super().__init__(fg_color=Colors.White)
+        super().__init__(fg_color=Colors.Coral)
 
         self.title("Doctobot")
-        self.state("zoomed")
+        self.iconbitmap("assets\imgs\Dbot logo.ico")
 
+        center(1280, 832, self)
+        self.resizable(False, False)
         env["DB"] = "main.db"
 
         self.conn = sqlite3.connect(env["DB"])
